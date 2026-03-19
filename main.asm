5b54fcab891a3d1840c6393672b46da66eed27a1
oartemiy <artemiy.obraztsov@yandex.ru>
2026-03-19 20:50:06 +0000

    03-5

--- main.asm
+++ main.asm
@@ -1,135 +1,68 @@
 %include "io.inc"
 
-;int fact(int)
-; fact:
-;     ...
 
-; int c(int n, int k)
+; uint reverse(uint n)
+reverse:
+    push ebp
+    mov ebp, esp
 
-; C:
-;     push ebp
-;     mov ebp, esp
-    
-;     mov eax, dword [ebp + 8] ; n
-;     mov ecx, dword [ebp + 12] ; k
+    mov eax, dword [ebp + 8] ; n
+    mov ecx, dword 0 ; ans
+    push ebx
+    mov ebx, dword 10
 
-;     cmp eax, ecx
-;     jl ans_zero
+reverse_begin:
+    mov edx, dword 0
+    div ebx
+    imul ecx, ebx
+    add ecx, edx
+    cmp eax, dword 0
+    jne reverse_begin
 
-;     test eax, eax
-;     je ans_1
-
-;     xor edx, edx
-;     dec eax
-;     push ecx
-;     push eax
-;     call C
-
-;     add edx, eax
-;     pop eax
-;     pop ecx
-
-;     dec ecx
-;     push ecx
-;     push eax
+reverse_end:
+    mov eax, ecx
+    pop ebx
+    mov esp, ebp
+    pop ebp
+    ret
 
 
-
-; ans_zero:
-;     mov eax, dword 0
-;     jmp end
-
-; ans_1:
-;     mov eax, dword 1
-
-; end:
-;     mov esp, ebp
-;     pop ebp
-;     ret
-
-
-
-; make_point:
-;     push ebp
-;     mov ebp, esp
-
-;     push ebx
-;     mov eax, dword [ebp + 8] ; hiden pointer
-;     mov ebx, dword [ebp + 12] ;a
-;     mov ecx, dword [ebp + 16] ;b
-;     mov edx, dword [ebp + 20] ;c
-
-;     mov dword[eax], ebx
-;     mov dword[eax + 4], ecx
-;     mov dword[eax + 8], ecx
-
-;     pop ebx
-;     mov esp, ebp
-;     pop ebp
-
-; Complex C_mult(Complex* a, Complex* b)
-; C_mult:
-;     push ebp
-;     mov ebp, esp
-
-;     push esi
-;     push edi
-;     push ebx
-
-;     mov esi, dword [ebp + 12]
-;     mov edi, dword [ebp + 16]
-
-;     mov eax, dword [esi] ; a->x
-;     mov ebx, dword [esi + 4]; a -> y
-;     mov ecx, dword [edi]; b->x
-;     mov edx, dword [edi + 4]; b->y
-
-;     mov esi, eax
-;     imul esi, ecx
-;     mov edi, ebx
-;     imul esi, edx
-;     sub esi, edi
-
-;     lea edi, [ebp + 8]
-;     mov dword [edi], esi
-
-;     mov esi, eax
-;     imul esi, edx
-
-;     mov edi, ebx
-;     imul edi, ecx
-
-;     add esi, edi
-
-;     lea edi, [ebp + 8]
-;     mov dword [edi + 4], esi
-
-;     pop ebx
-;     pop edi
-;     pop esi
-
-;     mov esp, ebp
-;     pop ebp
-;     ret
-
-
+section .bss
+    M resd 1
+    N resd 1
 
 section .text
 global main
 main:
-    push ebp
-    mov ebp, esp
-    sub esp, 12
+    GET_UDEC 4, [M]
+    GET_UDEC 4, [N]
+    mov edi, dword [M]
+    mov esi, dword [N]
+for_begin:
+    cmp esi, dword 0
+    jz for_end
+    push edi
+    call reverse
+    add edi, eax
+    add esp, dword 4
+    dec esi
+    jmp for_begin
 
-    lea eax, [esp]
+for_end:
+    push edi
+    call reverse
+    add esp, dword 4
+    cmp edi, eax
+    jz palindrome
+    PRINT_STRING `No`
+    NEWLINE
+    jmp end
 
-
-    push dword [c]
-    push dword [b]
-    push dword [a]
-    push eax
-    call make_point
-    
-    add esp, dword 16
+palindrome:
+    PRINT_STRING `Yes`
+    NEWLINE
+    PRINT_UDEC 4, edi
+    NEWLINE
+end:
     xor eax, eax
     ret
