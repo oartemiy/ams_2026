1186cc320864d2008a1c53b8b369c139db747890
oartemiy <artemiy.obraztsov@yandex.ru>
2026-04-01 16:32:22 +0000

    03-13

--- main.asm
+++ main.asm
@@ -1,164 +1,98 @@
 %include "io.inc"
 
-squareX2:
-    push ebp
-    mov ebp, esp
-    mov eax, dword [x1]
-    mov edx, dword [y2]
-    sub edx, dword [y3]
-    imul eax, edx
-    mov ecx, eax
-    mov eax, dword [x2]
-    mov edx, dword [y3]
-    sub edx, dword [y1]
-    imul eax, edx
-    add ecx, eax
-    mov eax, dword [x3]
-    mov edx, dword [y1]
-    sub edx, dword [y2]
-    imul eax, edx
-    add ecx, eax
-    mov eax, ecx
-    push eax
-    call absi
-    add esp, dword 4
-
-    mov esp, ebp
-    pop ebp
-    ret
-
-; uint abs(int a) -> a
-absi:
-    push ebp
-    mov ebp, esp
-    mov eax, dword [ebp + 8]
-    cdq
-    xor eax, edx
-    shr edx, 31
-    add eax, edx
-    mov esp, ebp
-    pop ebp
-    ret
-
-; int gcd(int a, int b) -> eax
-gcd:
-    push ebp
-    mov ebp, esp
-    mov eax, dword [ebp + 8]
-    mov ecx, dword [ebp + 12]
-while_begin:
-    cmp ecx, dword 0
-    ; PRINT_UDEC 4, ecx
-    ; PRINT_CHAR ' '
-    ; PRINT_UDEC 4, eax
-    ; NEWLINE
-    jz while_end
-    mov edx, dword 0
-    div ecx
-    mov eax, ecx
-    mov ecx, edx
-    jmp while_begin
-while_end:
-    mov esp, ebp
-    pop ebp
-    ret
-
-
-countGraniza:
+find_sequence:
     push ebp
     mov ebp, esp
     push edi
+    push ebx
     push esi
-    mov eax, dword [x1]
-    sub eax, dword [x2]
-    push eax
-    call absi
-    add esp, dword 4
-    mov esi, eax
+    sub esp, dword 4
 
-    mov eax, dword [y1]
-    sub eax, dword [y2]
-    push eax
-    call absi
-    add esp, dword 4
-    push eax
-    push esi
-    call gcd
-    add esp, dword 8
-    mov edi, eax
+    mov ecx, dword [N]
+    mov edi, 1
+    ; for avail
+initLoop:
+    mov dword [avail + edi*4], dword 1
+    inc edi
+    cmp edi, ecx
+    jle initLoop
 
-    mov eax, dword [x2]
-    sub eax, dword [x3]
-    push eax
-    call absi
-    add esp, dword 4
-    mov esi, eax
+    mov ebx, dword 1 ; current pos
+    mov eax, dword [M]
+    mov dword [ebp-4], eax ; M
 
-    mov eax, dword [y2]
-    sub eax, dword [y3]
-    push eax
-    call absi
-    add esp, dword 4
-    push eax
-    push esi
-    call gcd
-    add esp, dword 8
-    add edi, eax
 
-    mov eax, dword [x3]
-    sub eax, dword [x1]
-    push eax
-    call absi
-    add esp, dword 4
-    mov esi, eax
+mainLoop:
+    cmp ebx, dword [K]
+    jg end
+    ; pos - numbers are used
+    ; step = fact[N-pos] / fact[N-K]
+    mov eax, dword [N]
+    sub eax, ebx
+    mov ecx, dword [N]
+    sub ecx, dword [K]
 
-    mov eax, dword [y3]
-    sub eax, dword [y1]
-    push eax
-    call absi
-    add esp, dword 4
-    push eax
-    push esi
-    call gcd
-    add esp, dword 8
-    add edi, eax
-    mov eax, edi
+    mov eax, dword [fact + eax*4]
+    mov ecx, dword [fact + ecx*4]
+    mov edx, dword 0
+    div ecx
+    mov esi, eax ; step
 
+    ; find number
+    mov ecx, dword 1 ; x
+    mov edx, dword [ebp-4] ; cur M
+
+searchLoop:
+    cmp dword [avail + ecx*4], dword 0
+    jz next
+
+    cmp edx, esi
+    jle choose
+
+    sub edx, esi
+    jmp next
+
+choose:
+    PRINT_UDEC 4, ecx
+    cmp ebx, dword [K]
+    je noSpace
+    PRINT_CHAR ' '
+noSpace:
+    mov dword [avail + ecx*4], dword 0
+    mov dword [ebp-4], edx
+    inc ebx
+    jmp mainLoop
+
+next:
+    inc ecx
+    jmp searchLoop
+
+end:
+    add esp, dword 4
     pop esi
+    pop ebx
     pop edi
-    mov esp, ebp
     pop ebp
     ret
 
+section .data
+    fact dd 1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800
+
 section .bss
-x1 resd 1
-y1 resd 1
-x2 resd 1
-y2 resd 1
-x3 resd 1
-y3 resd 1
+    N resd 1
+    K resd 1
+    M resd 1
+    avail resd 11
 
 section .text
 global main
 main:
-    GET_DEC 4, [x1]
-    GET_DEC 4, [y1]
-    GET_DEC 4, [x2]
-    GET_DEC 4, [y2]
-    GET_DEC 4, [x3]
-    GET_DEC 4, [y3]
+    GET_DEC 4, [N]
+    GET_DEC 4, [K]
+    GET_DEC 4, [M]
 
-    call countGraniza
-    mov edi, eax
-    call squareX2
-    ; PRINT_UDEC 4, eax
-    ; NEWLINE
-    add eax, dword 2
-    sub eax, edi
-    mov edx, dword 0
-    mov ebx, dword 2
-    div ebx
-    PRINT_UDEC 4, eax
+    call find_sequence
+
     NEWLINE
     xor eax, eax
     ret
--- main.c
+++ main.c
@@ -1,25 +1,52 @@
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <sys/types.h>
-
 #include <stdio.h>
 #include <stdlib.h>
 
-uint rotate(uint num)
+int N, K, M;
+int used[11];
+int ans[10];
+int fact[11];
+
+void build(int pos, int m)
 {
-    uint a = 0;
-    while (num)
+    if (pos == K)
     {
-        a = a * 10 + num % 10;
-        num /= 10;
+        for (int i = 0; i < K; i++)
+        {
+            if (i > 0) printf(" ");
+            printf("%d", ans[i]);
+        }
+        printf("\n");
+        exit(0);
     }
-    return a;
+
+    for (int x = 1; x <= N; x++)
+    {
+        if (!used[x])
+        {
+            int cnt = fact[N - pos - 1] / fact[N - K];
+            if (m > cnt)
+            {
+                m -= cnt;
+            }
+            else
+            {
+                ans[pos] = x;
+                used[x] = 1;
+                build(pos + 1, m);
+                used[x] = 0;
+                return;
+            }
+        }
+    }
 }
 
-
-int main(void)
+int main()
 {
-    int a;
-    int b;
-    printf("%p\n%p\n", &a, &b);
+    scanf("%d %d %d", &N, &K, &M);
+
+    fact[0] = 1;
+    for (int i = 1; i <= 10; i++) fact[i] = fact[i - 1] * i;
+
+    build(0, M);
+    return 0;
 }
